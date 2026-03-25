8869aaf7d4ba3b5af12d18f3a09d6ad6b1023758
Peraça <rodrigoperaca.aluno@unipampa.edu.br>
2026-03-25 11:11:51 -0300

    Correcao em maps e tell

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 
 const FACILITIES = [
   {
-    id: 'ubs-central',
-    name: 'UBS Central Dr. Mário Totta',
-    type: 'UBS',
-    neighborhood: 'Centro',
-    address: 'Av. Tupy Silveira, 1432 - Centro, Bagé - RS',
-    hours: 'Seg a Sex · 7h30 às 17h',
-    phone: '(53) 3240-4300',
+    id: 'ESF-Arvorezinha',
+    name: 'ESF Arvorezinha',
+    type: 'ESF',
+    neighborhood: 'Tarumã',
+    address: 'Rua Pompilio Nunes,275 - Tarumã, Bagé - RS',
+    hours: 'Seg a Sex · 13h30 às 17h30',
+    phone: '(53) 3241-1588',
     services: [
       'Clínica Geral',
       'Sala de Vacinas',
@@ -17,18 +17,18 @@ const FACILITIES = [
       'Dispensação de medicamentos',
     ],
     googleMaps:
-      'https://www.google.com/maps/search/?api=1&query=Av.+Tupy+Silveira%2C+1432%2C+Bag%C3%A9+-+RS',
+      'https://maps.app.goo.gl/QEk2Kc8hfdkmWR728',
     notes:
       'Ponto de referência para atendimentos de média complexidade e marcações do e-SUS.',
   },
   {
-    id: 'esf-marcos',
-    name: 'ESF Dr. Marcos Freire',
+    id: 'ESF Centro-Social-Urbano',
+    name: 'ESF Centro Social Urbano Áttila Taborda',
     type: 'ESF',
-    neighborhood: 'Getúlio Vargas',
-    address: 'Rua General Osório, 215 - bairro Getúlio Vargas',
-    hours: 'Seg a Sex · 8h às 16h',
-    phone: '(53) 3240-4412',
+    neighborhood: 'Pedras Brancas',
+    address: 'Rua General Osório, 215 - bairro Pedras Brancas, Bagé - RS',
+    hours: 'Seg a Sex · 8h às 18h',
+    phone: '(53) 3241-0042',
     services: [
       'Atendimento multiprofissional da ESF',
       'Pré-natal',
@@ -36,36 +36,36 @@ const FACILITIES = [
       'Visitas domiciliares',
     ],
     googleMaps:
-      'https://www.google.com/maps/search/?api=1&query=Rua+General+Os%C3%B3rio%2C+215%2C+Bag%C3%A9+-+RS',
+      'https://maps.app.goo.gl/6NsLJbufWFrTmJTD8',
     notes:
       'Equipe integrada ao Programa Universidade Aberta do SUS com participação da Unipampa.',
   },
   {
-    id: 'esf-ivai',
-    name: 'ESF Ivai Ferreira',
+    id: 'ESF-Damé',
+    name: 'ESF Damé',
     type: 'ESF',
-    neighborhood: 'Pedras Brancas',
-    address: 'Rua João Telles, 980 - Pedras Brancas',
-    hours: 'Seg a Sex · 8h às 17h',
-    phone: '(53) 3240-4521',
+    neighborhood: 'Centro',
+    address: 'Rua 93 - Centro, Bagé - RS',
+    hours: 'Seg a Sex · 13h30 às 17h30',
+    phone: '(53) 3242-2476',
     services: [
       'Estratégia Saúde da Família',
       'Grupos de Hipertensão/Diabetes',
       'Acolhimento odontológico',
     ],
     googleMaps:
-      'https://www.google.com/maps/search/?api=1&query=Rua+Jo%C3%A3o+Telles%2C+980%2C+Bag%C3%A9+-+RS',
+      'https://maps.app.goo.gl/8yeCTE56M6Vow9Be7',
     notes:
       'Foco em vigilância ativa com apoio dos laboratórios de Engenharia de Computação da Unipampa.',
   },
   {
-    id: 'ubs-damasceno',
-    name: 'UBS Damasceno',
+    id: 'UBS-Ivo-Ferronato',
+    name: 'UBS Ivo Ferronato',
     type: 'UBS',
-    neighborhood: 'Damasceno',
-    address: 'Av. Santa Tecla, 2900 - Damasceno',
-    hours: 'Seg a Sex · 8h às 18h',
-    phone: '(53) 3240-4707',
+    neighborhood: 'Industrial I',
+    address: 'R. Luiz Adão Médici, 2990 - Industrial I, Bagé - RS',
+    hours: 'Seg a Sex · 7h30 às 12h, 13h30 às 17h',
+    phone: '(53) 3247-6004',
     services: [
       'Clínica Geral',
       'Saúde Bucal',
@@ -73,35 +73,35 @@ const FACILITIES = [
       'Curativos e procedimentos',
     ],
     googleMaps:
-      'https://www.google.com/maps/search/?api=1&query=Av.+Santa+Tecla%2C+2900%2C+Bag%C3%A9+-+RS',
+      'https://maps.app.goo.gl/dMaW6zD4dL3kMNqC6',
     notes: 'Integra o eixo de telessaúde experimental com alunos da Engenharia de Computação.',
   },
   {
-    id: 'esf-stand',
-    name: 'ESF Stand do Amaral',
-    type: 'ESF',
-    neighborhood: 'Stand do Amaral',
-    address: 'Rua 20 de Setembro, 555 - Stand do Amaral',
-    hours: 'Seg a Sex · 8h às 16h',
-    phone: '(53) 3240-4820',
+    id: 'UBS-Passo-das-Pedras',
+    name: 'UBS Passo das Pedras',
+    type: 'UBS',
+    neighborhood: 'Castro Alves',
+    address: 'R. Wilsom Rosa da Cruz, 135 - Castro Alves, Bagé - RS',
+    hours: 'Seg a Sex · 13h30 às 17h',
+    phone: '(53) 3240-6760',
     services: [
       'Estratégia Saúde da Família',
       'Acompanhamento de gestantes',
       'Assistência social e NASF',
     ],
     googleMaps:
-      'https://www.google.com/maps/search/?api=1&query=Rua+20+de+Setembro%2C+555%2C+Bag%C3%A9+-+RS',
+      'https://maps.app.goo.gl/LCpCQfUmqqijEz2d6',
     notes:
       'Equipe com agentes comunitários conectados ao observatório Pampa Saúde da Unipampa.',
   },
   {
-    id: 'ubs-industrial',
-    name: 'UBS Bairro Industrial',
+    id: 'UBS-ESF-Prado-Velho',
+    name: 'UBS ESF Prado Velho',
     type: 'UBS',
-    neighborhood: 'Industrial',
-    address: 'Rua Professor Aníbal Benevides, 35 - Industrial',
-    hours: 'Seg a Sex · 7h às 18h',
-    phone: '(53) 3240-4901',
+    neighborhood: 'Prado Velho',
+    address: 'R. 738, 140 - Prado Velho, Bagé - RS',
+    hours: 'Seg a Sex · 7h30 às 17h',
+    phone: '(53) 3247-3876',
     services: [
       'Clínica Geral',
       'Pediatria',
@@ -109,7 +109,387 @@ const FACILITIES = [
       'Serviços laboratoriais básicos',
     ],
     googleMaps:
-      'https://www.google.com/maps/search/?api=1&query=Rua+Professor+An%C3%ADbal+Benevides%2C+35%2C+Bag%C3%A9+-+RS',
+      'https://maps.app.goo.gl/RDvrhRxRKW7EoE9P7',
+    notes:
+      'Piloto de indicadores inteligentes desenvolvido pelo curso de Engenharia de Computação/Unipampa.',
+  },
+  {
+    id: 'UBS-ESF-São-Martin',
+    name: 'UBS ESF São Martin',
+    type: 'UBS',
+    neighborhood: 'São Martins',
+    address: 'R. Onze, 265 - São Martins, Bagé - RS',
+    hours: 'Seg a Sex · 13h30 às 17h30',
+    phone: '(53) 3247-1385',
+    services: [
+      'Clínica Geral',
+      'Pediatria',
+      'Saúde Mental',
+      'Serviços laboratoriais básicos',
+    ],
+    googleMaps:
+      'https://maps.app.goo.gl/eztGbUAvzWFctPb97',
+    notes:
+      'Piloto de indicadores inteligentes desenvolvido pelo curso de Engenharia de Computação/Unipampa.',
+  },
+  {
+    id: 'UBS-ESF-Floresta',
+    name: 'UBS ESF Floresta',
+    type: 'UBS',
+    neighborhood: 'Floresta',
+    address: 'Estr. do Passo do Apertado, 484-618 - Floresta, Bagé - RS',
+    hours: 'Seg a Sex · 7h30 às 17h30',
+    phone: '(53) ---------',
+    services: [
+      'Clínica Geral',
+      'Pediatria',
+      'Saúde Mental',
+      'Serviços laboratoriais básicos',
+    ],
+    googleMaps:
+      'https://maps.app.goo.gl/BSDQySfhBWTYyYHW7',
+    notes:
+      'Piloto de indicadores inteligentes desenvolvido pelo curso de Engenharia de Computação/Unipampa.',
+  },
+  {
+    id: 'UBS-ESF-Morgado-Rosa',
+    name: 'UBS ESF Morgado Rosa',
+    type: 'UBS',
+    neighborhood: 'Habilitar Brasil',
+    address: 'R. Valdemar Milan, 690 - Habitar Brasil, Bagé - RS, 96418-020',
+    hours: 'Seg a Sex · 13h30 às 17h30',
+    phone: '(53) 3242-8518',
+    services: [
+      'Clínica Geral',
+      'Pediatria',
+      'Saúde Mental',
+      'Serviços laboratoriais básicos',
+    ],
+    googleMaps:
+      'https://maps.app.goo.gl/nutertoSDxGP8s5YA',
+    notes:
+      'Piloto de indicadores inteligentes desenvolvido pelo curso de Engenharia de Computação/Unipampa.',
+  },
+  {
+    id: 'UBS-ESF-Santa-Cecília',
+    name: 'UBS ESF Santa Cecília',
+    type: 'UBS',
+    neighborhood: 'Menino Deus',
+    address: 'R. Armando Xavier Azambuja - Menino Deus, Bagé - RS,',
+    hours: 'Seg a Sex · 13h30 às 17h30',
+    phone: '(53) 3247-1356',
+    services: [
+      'Clínica Geral',
+      'Pediatria',
+      'Saúde Mental',
+      'Serviços laboratoriais básicos',
+    ],
+    googleMaps:
+      'https://maps.app.goo.gl/k1iW7i8hjvxUnwz36',
+    notes:
+      'Piloto de indicadores inteligentes desenvolvido pelo curso de Engenharia de Computação/Unipampa.',
+  },
+  {
+    id: 'UBS-Malafáia',
+    name: 'UBS Malafáia',
+    type: 'UBS',
+    neighborhood: 'Industrial I',
+    address: 'R. Odilson Álvares, 1935 - Industrial I, Bagé - RS,',
+    hours: 'Seg a Sex · 7h30 às 11h30, 13h30 às 16h30',
+    phone: '(53) 3242-3016',
+    services: [
+      'Clínica Geral',
+      'Pediatria',
+      'Saúde Mental',
+      'Serviços laboratoriais básicos',
+    ],
+    googleMaps:
+      'https://maps.app.goo.gl/e1fgdUYuPsN4Am79A',
+    notes:
+      'Piloto de indicadores inteligentes desenvolvido pelo curso de Engenharia de Computação/Unipampa.',
+  },
+  {
+    id: 'ESF-Dois-Irmãos',
+    name: 'Posto de Saúde ESF Dois Irmãos',
+    type: 'ESF',
+    neighborhood: 'São Judas',
+    address: 'R. Nossa Sra. dos Navegantes, 82 - São Judas, Bagé - RS,',
+    hours: 'Seg a Sex · 8h às 17h',
+    phone: '(53) 3241-0774',
+    services: [
+      'Clínica Geral',
+      'Pediatria',
+      'Saúde Mental',
+      'Serviços laboratoriais básicos',
+    ],
+    googleMaps:
+      'https://maps.app.goo.gl/BBJ7h5F3wsfn1jZg6',
+    notes:
+      'Piloto de indicadores inteligentes desenvolvido pelo curso de Engenharia de Computação/Unipampa.',
+  },
+  {
+    id: 'UBS-São-Bernardo',
+    name: 'UBS São Bernardo',
+    type: 'UBS',
+    neighborhood: 'Getúlio Vargas',
+    address: 'R. Barão de Itaqui, 1010 - Getúlio Vargas, Bagé - RS,',
+    hours: 'Seg a Sex · 7h30 às 12h, 13h30 às 17h',
+    phone: '(53) 3241-7990',
+    services: [
+      'Clínica Geral',
+      'Pediatria',
+      'Saúde Mental',
+      'Serviços laboratoriais básicos',
+    ],
+    googleMaps:
+      'https://maps.app.goo.gl/pADDdEhcNWL21ry86',
+    notes:
+      'Piloto de indicadores inteligentes desenvolvido pelo curso de Engenharia de Computação/Unipampa.',
+  },
+  {
+    id: 'Posto-de-Saúde-Sá-Mommany',
+    name: 'Posto de Saúde Sá Mommany',
+    type: 'ESF',
+    neighborhood: 'Getúlio Vargas',
+    address: 'R. Carlos Barbosa, 1385 - Getúlio Vargas, Bagé - RS',
+    hours: 'Seg a Sex · 8h às 11h',
+    phone: '(53) 3242-5390',
+    services: [
+      'Clínica Geral',
+      'Pediatria',
+      'Saúde Mental',
+      'Serviços laboratoriais básicos',
+    ],
+    googleMaps:
+      'https://maps.app.goo.gl/8uUGMHjPZHDhPH1P8',
+    notes:
+      'Piloto de indicadores inteligentes desenvolvido pelo curso de Engenharia de Computação/Unipampa.',
+  },
+  {
+    id: 'UBS-CAIC',
+    name: 'UBS CAIC',
+    type: 'UBS',
+    neighborhood: 'São Jorge',
+    address: 'Av. Tupi Silveira, 141 - São Jorge, Bagé - RS',
+    hours: 'Seg a Sex · 7h30 às 16h45',
+    phone: '(53) 3241-1552',
+    services: [
+      'Clínica Geral',
+      'Pediatria',
+      'Saúde Mental',
+      'Serviços laboratoriais básicos',
+    ],
+    googleMaps:
+      'https://maps.app.goo.gl/U4RcQHBZsaN48xGQ9',
+    notes:
+      'Piloto de indicadores inteligentes desenvolvido pelo curso de Engenharia de Computação/Unipampa.',
+  },
+  {
+    id: 'Posto-de-Saúde-Camilo-Gomes',
+    name: 'Posto de Saúde Camilo Gomes',
+    type: 'ESF',
+    neighborhood: 'Centro',
+    address: 'R. Fabrício Pilar, 1201 - Centro, Bagé - RS',
+    hours: 'Seg a Sex · 7h30 às 17h30',
+    phone: '(53) 3242-2433',
+    services: [
+      'Clínica Geral',
+      'Pediatria',
+      'Saúde Mental',
+      'Serviços laboratoriais básicos',
+    ],
+    googleMaps:
+      'https://maps.app.goo.gl/MuJGhk3iQ4TbA8B87',
+    notes:
+      'Piloto de indicadores inteligentes desenvolvido pelo curso de Engenharia de Computação/Unipampa.',
+  },
+  {
+    id: 'Centro-Referência-em-Tuberculose',
+    name: 'Centro de Referência em Tuberculose',
+    type: 'UBS',
+    neighborhood: 'Centro',
+    address: 'R. Floriano Peixoto, 1752 - Centro, Santa Maria - RS',
+    hours: 'Seg a Sex · *** às ***',
+    phone: '(53) 3921-7060',
+    services: [
+      'Clínica Geral',
+      'Pediatria',
+      'Saúde Mental',
+      'Serviços laboratoriais básicos',
+    ],
+    googleMaps:
+      'https://maps.app.goo.gl/CnmwW8nMN1pRsv3m7',
+    notes:
+      'Piloto de indicadores inteligentes desenvolvido pelo curso de Engenharia de Computação/Unipampa.',
+  },
+  {
+    id: 'Mathilde-Fayad-CAPS-I',
+    name: 'Mathilde Fayad - CAPS I',
+    type: 'CAPS',
+    neighborhood: 'Centro',
+    address: 'R. Caetano Gonçalves, 811 - Centro, Bagé - RS, 96400-040',
+    hours: 'Seg a Sex · *** às ***',
+    phone: '(53) 3242-1495',
+    services: [
+      'Clínica Geral',
+      'Pediatria',
+      'Saúde Mental',
+      'Serviços laboratoriais básicos',
+    ],
+    googleMaps:
+      'https://maps.app.goo.gl/sbki39gvefkDmT8D8',
+    notes:
+      'Piloto de indicadores inteligentes desenvolvido pelo curso de Engenharia de Computação/Unipampa.',
+  },
+  {
+    id: 'CAPS-II-Saúde-Mental',
+    name: 'CAPS II - Saúde Mental',
+    type: 'CAPS',
+    neighborhood: 'Centro',
+    address: 'Av. Mal. Floriano, 1499 - Centro, Bagé - RS',
+    hours: 'Seg a Sex · 8h às 17h',
+    phone: '(53) 3247-3248',
+    services: [
+      'Clínica Geral',
+      'Pediatria',
+      'Saúde Mental',
+      'Serviços laboratoriais básicos',
+    ],
+    googleMaps:
+      'https://maps.app.goo.gl/em1ruGWbxFfw3gqq5',
+    notes:
+      'Piloto de indicadores inteligentes desenvolvido pelo curso de Engenharia de Computação/Unipampa.',
+  },
+  {
+    id: 'CAPS-AD',
+    name: 'CAPS AD',
+    type: 'CAPS',
+    neighborhood: 'São Jorge',
+    address: 'Av. Gen. Osório, 320 - São Jorge, Bagé - RS',
+    hours: 'Seg a Sex · *** às ***',
+    phone: '(53) 3247-7243',
+    services: [
+      'Clínica Geral',
+      'Pediatria',
+      'Saúde Mental',
+      'Serviços laboratoriais básicos',
+    ],
+    googleMaps:
+      'https://maps.app.goo.gl/NgAXio6RKQUsUDxp7',
+    notes:
+      'Piloto de indicadores inteligentes desenvolvido pelo curso de Engenharia de Computação/Unipampa.',
+  },
+  {
+    id: 'SAIS-Bagé',
+    name: 'SAIS Bagé',
+    type: 'SAIS',
+    neighborhood: 'Centro',
+    address: 'R. Fabrício Pilar, 1201 - Centro, Bagé - RS',
+    hours: 'Seg a Sex · 7h30 às 17h',
+    phone: '(53) 3242-2433',
+    services: [
+      'Clínica Geral',
+      'Pediatria',
+      'Saúde Mental',
+      'Serviços laboratoriais básicos',
+    ],
+    googleMaps:
+      'https://maps.app.goo.gl/h4n6jqa2fyJgd4qm8',
+    notes:
+      'Piloto de indicadores inteligentes desenvolvido pelo curso de Engenharia de Computação/Unipampa.',
+  },
+  {
+    id: 'UBS-ESF-Doutor-Jorge-Sui-e-Grillo',
+    name: 'UBS ESF Doutor Jorge Sui e Grillo',
+    type: 'UBS',
+    neighborhood: 'Centro',
+    address: 'Av. Gen. Mallet - Centro, Bagé - RS',
+    hours: 'Seg a Sex · 13h30 às 17h30',
+    phone: '(53) 3242-7755',
+    services: [
+      'Clínica Geral',
+      'Pediatria',
+      'Saúde Mental',
+      'Serviços laboratoriais básicos',
+    ],
+    googleMaps:
+      'https://maps.app.goo.gl/NgAXio6RKQUsUDxp7',
+    notes:
+      'Piloto de indicadores inteligentes desenvolvido pelo curso de Engenharia de Computação/Unipampa.',
+  },
+  {
+    id: 'SAMU-Base-Bagé',
+    name: 'SAMU Base de Bagé',
+    type: 'SAMU',
+    neighborhood: 'Getúlio Vargas',
+    address: 'Av. Santa Tecla, 299 - Getúlio Vargas, Bagé - RS',
+    hours: 'Seg a Sex · *** às ***',
+    phone: '(53) 99998-6283',
+    services: [
+      'Clínica Geral',
+      'Pediatria',
+      'Saúde Mental',
+      'Serviços laboratoriais básicos',
+    ],
+    googleMaps:
+      'https://maps.app.goo.gl/a6E4vfdJ8trZKajy9',
+    notes:
+      'Piloto de indicadores inteligentes desenvolvido pelo curso de Engenharia de Computação/Unipampa.',
+  },
+  {
+    id: 'UBS-Ivone',
+    name: 'UBS Ivone',
+    type: 'UBS',
+    neighborhood: 'Castro Alves',
+    address: 'R. Cecira Falace Saraíva, 514 - Castro Alves, Bagé - RS',
+    hours: 'Seg a Sex · 13h30 às 17h30',
+    phone: '(53) 3247-3273',
+    services: [
+      'Clínica Geral',
+      'Pediatria',
+      'Saúde Mental',
+      'Serviços laboratoriais básicos',
+    ],
+    googleMaps:
+      'https://maps.app.goo.gl/Bj6HJb9b6EXh9XbPA',
+    notes:
+      'Piloto de indicadores inteligentes desenvolvido pelo curso de Engenharia de Computação/Unipampa.',
+  },
+  {
+    id: 'UPA-Padre-Honorino',
+    name: 'UPA Padre Honorino',
+    type: 'UPA',
+    neighborhood: 'Getúlio Vargas',
+    address: 'Rua Felix Contreiras Rodrigues, 69 - Getúlio Vargas, Bagé - RS',
+    hours: 'Todos os dias · 24h',
+    phone: '(53) 3247-7243',
+    services: [
+      'Clínica Geral',
+      'Pediatria',
+      'Saúde Mental',
+      'Serviços laboratoriais básicos',
+    ],
+    googleMaps:
+      'https://maps.app.goo.gl/na7WGyFayy58ihoB6',
+    notes:
+      'Piloto de indicadores inteligentes desenvolvido pelo curso de Engenharia de Computação/Unipampa.',
+  },
+  {
+    id: 'Hospital-Universitario-Dr-Mário-Araújo',
+    name: 'Hospital Universitário Dr. Mário Araújo',
+    type: 'Hospital',
+    neighborhood: 'Popular',
+    address: 'R. Gen. Flores da Cunha, 169 - Popular, Bagé - RS',
+    hours: 'Todos os dias · 7h30 às 12h, 13h30 às 18h',
+    phone: '(53) 3247-7243',
+    services: [
+      'Clínica Geral',
+      'Pediatria',
+      'Saúde Mental',
+      'Serviços laboratoriais básicos',
+    ],
+    googleMaps:
+      'https://maps.app.goo.gl/Qiuj3jrw2XBYVpvh6',
     notes:
       'Piloto de indicadores inteligentes desenvolvido pelo curso de Engenharia de Computação/Unipampa.',
   },
@@ -119,6 +499,10 @@ const TYPE_LABELS = {
   all: 'Todas as unidades',
   UBS: 'Unidade Básica de Saúde',
   ESF: 'Estratégia Saúde da Família',
+  CAPS: 'Centro de Atenção Psicossocial',
+  SAIS: 'Serviço de Atenção Integral à Saúde',
+  UPA: 'Unidade de Pronto Atendimento',
+  Hospital: 'Hospital',
 };
 
 const INSTITUTION_LOGOS = [
