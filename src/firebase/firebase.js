528bc23d0c39c5d35548e4c7763f6c8aec15ab69
Peraça <rodrigoperaca.aluno@unipampa.edu.br>
2026-05-29 10:45:42 -0300

    correção de segurança

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 
 //Credencial real do firebase do projeto Pampa Saúde 
 const firebaseConfig = {
-  apiKey: "AIzaSyA_BZSRZFImnVwogdFvkdbufcIEKMjLYBY",
-  authDomain: "pampa-saude.firebaseapp.com",
-  projectId: "pampa-saude",
-  storageBucket: "pampa-saude.firebasestorage.app",
-  messagingSenderId: "403037160519",
-  appId: "1:403037160519:web:b072da322c05bf61631d62",
-  measurementId: "G-BKG2Z3SS6Z"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Inicializa Firebase
